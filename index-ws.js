ae966f5ce73f95e9c9edf4cfd43159cd4ad69b2b
rashawn yashadhana <shawnyashadhana8@gmail.com>
2024-10-14 15:37:29 +0700

    betulin index 2 2 nya

--- index-ws.js
+++ index-ws.js
@@ -1,38 +1,51 @@
 const express = require('express');
-const { ServerResponse } = require('http');
 const server = require('http').createServer();
 const app = express();
+const PORT = 3000;
 
-
-app.get('/',function(req,res){
-    res.sendFile('index.html',{root: __dirname});
-
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: __dirname});
 });
 
-server.on('request',app)
-server.listen(3000,function() { console.log('server started on port 3000'); });
+server.on('request', app);
 
-/**begin websocket */
+server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+/** Websocket **/
 const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({server: server});
-wss.on('connection', function connection(ws){
-    const numClients = wss.clients.size;
-    console.log('Clients connected',numClients);
 
-    wss.broadcast(`Current visitors: ${numClients}`);
-    if(ws.readyState === ws.OPEN){
-        ws.send("Welcome to My Server");
-    }
+const wss = new WebSocketServer({ server: server });
 
-    ws.on('close',function close(){
-        console.log("A client has disconnected");
-    });
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
 
+  console.log('clients connected: ', numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('welcome!');
+  }
+
+  ws.on('close', function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log('A client has disconnected');
+  });
+
+  ws.on('error', function error() {
+    //
+  });
 });
 
-wss.broadcast = function broadcast(data){
-   wss.clients.forEach(function each(client){
-        client.send(data);
-    });
-}
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log('Broadcasting: ', data);
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+/** End Websocket **/
