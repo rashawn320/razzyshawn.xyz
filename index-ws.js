209138d117cc7d2278e9f726105d11b518b10cea
rashawn yashadhana <shawnyashadhana8@gmail.com>
2024-10-14 15:59:59 +0700

    psad

--- index-ws.js
+++ index-ws.js
@@ -3,23 +3,17 @@ const server = require('http').createServer();
 const app = express();
 const PORT = 3000;
 
-// Serve the index.html file
 app.get('/', function(req, res) {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile('index.html', {root: __dirname});
 });
 
-// Listen for requests on all network interfaces
 server.on('request', app);
 
-// Start the server on the specified port
-server.listen(PORT, '0.0.0.0', function () {
-  console.log('Listening on ' + PORT);
-});
+server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
-// Create a WebSocket server, using the same server instance
 const wss = new WebSocketServer({ server: server });
 
 wss.on('connection', function connection(ws) {
@@ -27,7 +21,6 @@ wss.on('connection', function connection(ws) {
 
   console.log('clients connected: ', numClients);
 
-  // Broadcast the current number of clients connected
   wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
@@ -52,9 +45,7 @@ wss.on('connection', function connection(ws) {
 wss.broadcast = function broadcast(data) {
   console.log('Broadcasting: ', data);
   wss.clients.forEach(function each(client) {
-    if (client.readyState === client.OPEN) {
-      client.send(data);
-    }
+    client.send(data);
   });
 };
 /** End Websocket **/
