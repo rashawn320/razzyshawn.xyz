96663458c5421f6af001e799d22d28785730ed8e
rashawn yashadhana <shawnyashadhana8@gmail.com>
2024-10-15 13:17:50 +0700

    add db

--- index-ws.js
+++ index-ws.js
@@ -8,9 +8,17 @@ app.get('/', function(req, res) {
 });
 
 server.on('request', app);
-
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+process.on('SIGINT',() =>{
+    console.log('signint')
+    wss.clients.forEach(function each(client){
+        client.close();
+    })
+    server.close(() => {
+        shutdownDB();
+    })
+})
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
@@ -24,6 +32,9 @@ wss.on('connection', function connection(ws) {
 if (ws.readyState === ws.OPEN) {
     ws.send('welcome!');
 }
+db.run(`INSERT INTO visitors (count ,time)
+    VALUES (${numClients} , datetime('now'))
+    `);
 
 ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
@@ -47,3 +58,28 @@ wss.broadcast = function broadcast(data) {
 });
 };
 /** End Websocket **/
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:')
+
+db.serialize( () => {
+    db.run(`
+        CREATE TABLE visitors(
+            count INTEGER,
+            time TEXT
+        )
+        
+        `)
+});
+
+function getCounts(){
+    db.each("SELECT * FROM visitors", (err,row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB(){
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
+}
